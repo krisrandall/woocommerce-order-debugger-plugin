8c6d37437e651d978a00861d13c1b914fc1aaed3
krisrandall <kris.randall@gmail.com>
2017-04-17 12:47:18 +1000

    do almost everything except actually execute the code

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ and I needed a way to easily do some interactive debugging.
 
 This plugin lets you enter an order number and then enter any arbitrary PHP code to apply to the order.
 
+NOTE: This plugin is only intended as a deubgging aid for competent developers - do not use it if you don't know what you are doing, and do not leave it on a live site.
+
+
 ### Based upon : https://docs.woocommerce.com/wc-apidocs/class-WC_Order.html
 
 
--- settings-class.php
+++ settings-class.php
@@ -19,59 +19,99 @@ class Settings {
     if ( is_admin() ) {
 
         // create custom plugin settings menu
-        add_action('admin_menu', 'my_cool_plugin_create_menu');
+        add_action('admin_menu', 'wc_order_debugger_plugin_create_menu');
 
-        function my_cool_plugin_create_menu() {
+        function wc_order_debugger_plugin_create_menu() {
 
             //create new top-level menu
-            add_menu_page('My Cool Plugin Settings', 'Cool Settings', 'administrator', __FILE__, 'my_cool_plugin_settings_page' , plugins_url('/images/icon.png', __FILE__) );
+            add_menu_page('WC_Order Debug', 'WC_Order Debug', 'administrator', __FILE__, 'wc_order_debugger_plugin_settings_page' , plugins_url('/images/icon.png', __FILE__) );
 
             //call register settings function
-            add_action( 'admin_init', 'register_my_cool_plugin_settings' );
+            add_action( 'admin_init', 'register_wc_order_debugger_plugin_settings' );
         }
 
 
-        function register_my_cool_plugin_settings() {
+        function register_wc_order_debugger_plugin_settings() {
             //register our settings
-            register_setting( 'my-cool-plugin-settings-group', 'new_option_name' );
-            register_setting( 'my-cool-plugin-settings-group', 'some_other_option' );
-            register_setting( 'my-cool-plugin-settings-group', 'option_etc' );
+            register_setting( 'woocommerce-order-debugger-plugin-settings-group', 'order_number' );
+            register_setting( 'woocommerce-order-debugger-plugin-settings-group', 'code_to_run' );
+
         }
 
-        function my_cool_plugin_settings_page() {
-        ?>
-        <div class="wrap">
-        <h1>Your Plugin Name</h1>
+        function wc_order_debugger_plugin_settings_page() {
 
-        <form method="post" action="options.php">
-            <?php settings_fields( 'my-cool-plugin-settings-group' ); ?>
-            <?php do_settings_sections( 'my-cool-plugin-settings-group' ); ?>
-            <table class="form-table">
-                <tr valign="top">
-                <th scope="row">New Option Name</th>
-                <td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
-                </tr>
-                
-                <tr valign="top">
-                <th scope="row">Some Other Option</th>
-                <td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
-                </tr>
-                
-                <tr valign="top">
-                <th scope="row">Options, Etc.</th>
-                <td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
-                </tr>
-            </table>
-            
-            <?php submit_button(); ?>
+            if (!Settings::woocommerce_enabled()) {
+                echo "You must have WooCommerce installed and enabled in order to use this plugin";
+            } else {
 
-        </form>
-        </div>
-        <?php 
+                $code_val = get_option('code_to_run');
+                if ($code_val=='') {
+                    $code_val = '$order = new WC_Order( get_option(\'order_number\') );'.
+                                "\n\n\n\n\n\n\n".
+                                'var_dump( $order );';
+                }
+
+                $order_num = esc_attr( get_option('order_number') );
+
+            ?>
+            <div class="wrap">
+            <h1>WC_Order Debugger</h1>
+
+            <form method="post" action="options.php">
+                <?php settings_fields( 'woocommerce-order-debugger-plugin-settings-group' ); ?>
+                <?php do_settings_sections( 'woocommerce-order-debugger-plugin-settings-group' ); ?>
+                <table class="form-table">
+                    <tr valign="top">
+                    <th scope="row" title="Enter an existing order number">Order Number</th>
+                    <td>
+                        <input type="text" name="order_number" value="<?php echo $order_num; ?>" />
+                        <a href="<?php echo admin_url('edit.php?post_type=shop_order'); ?>">List Orders</a>
+                    </td>
+                    </tr>
+                    
+                    <tr valign="top">
+                    <th scope="row">PHP Code to Execute</th>
+                    <td><textarea rows="11" cols="80" name="code_to_run"><?php echo esc_attr( $code_val ); ?></textarea></td>
+                    </tr>
+
+                </table>
+                
+                <?php submit_button('Run Code'); ?>
+
+                <!-- And here we execute the code -->
+
+                <?php
+
+                if (!$order_num) {
+                    $code_output = "Enter an Order Number and click Run Code in order to see the output";
+                } else {
+
+                    $code_output = 'TODO!!';
+
+                }
+                ?>
+
+                <table class="form-table">
+                
+                    <tr valign="top">
+                    <th scope="row">Output</th>
+                    <td><pre><?php echo esc_attr( $code_output ); ?></pre></td>
+                    </tr>
+
+                </table>
+                
+            </form>
+            </div>
+            <?php 
+            }
         } 
 
     }
 
+  }
+
+  static function woocommerce_enabled() {
+    return in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) );
   }
 
 }
